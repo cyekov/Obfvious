48b5bc120ef6e5d5117711e261de0b20115da85d
nimrodpar <nimrodpar@gmail.com>
2020-12-01 00:27:03 +0200

    latest

--- clang/lib/CodeGen/ShadowObfuscator.h
+++ clang/lib/CodeGen/ShadowObfuscator.h
@@ -14,6 +14,7 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Constant.h"
 #include "llvm/IR/GlobalVariable.h"
+#include "llvm/IR/Operator.h"
 #include "llvm/IR/LLVMContext.h"
 
 namespace clang {
@@ -25,12 +26,17 @@ class ShadowObfuscator {
 private:
 
     llvm::Module &TheModule;
+    unsigned char Seed;
 
     void DoGlobalString(llvm::GlobalVariable &Glob);
     llvm::Constant * EncodeString(llvm::StringRef StrVal);
+    void AddGEPOpDecodeLogicForUser(llvm::GEPOperator *GEPOp, llvm::User *U);
 
 public:
-    ShadowObfuscator(llvm::Module &M) : TheModule(M) {}
+    ShadowObfuscator(llvm::Module &M) : TheModule(M) {
+      srand(time(0));
+      Seed = (unsigned char)rand();
+    }
 
     void Obfuscate();
 
